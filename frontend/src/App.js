9876943f3501d9f5002f191640c6b9601329ce32
angdoo0 <143698028+angdoo0@users.noreply.github.com>
2024-01-23 17:54:49 +0900

    🎨 , ♻️ : APP.js 혜림님 테스트 코드 추가, SideBar,js float 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import SideBar from './component/common/SideBar';
+import { BtnBlack, BtnBlue, BtnWhite, BtnFilter, InputBar, SearchInput, SearchSelectBox, StepBar } from './component/common/module';
 
 function App() {
   return (
@@ -12,6 +13,21 @@ function App() {
         </Routes>
       </Router>
       <h1>편의를 위해 만들어둠.  서버 정상 작동중</h1>
+      <div>
+
+        <BtnBlack value={'검정'} link={'/black'} />
+        <BtnBlue value={'파랑'} link={'/blue'} />
+        <BtnWhite value={'흰색'} link={'/white'} />
+        <BtnFilter value={'완료'} link={'/complete'} />
+        <InputBar placeholderMsg={'값을 입력해주세요...'} />
+        <SearchInput />
+        {/* <SearchSelectBox value={'가', '나', '다'} />  */}
+        <StepBar stateNum={1} />
+        <StepBar stateNum={2} />
+        <StepBar stateNum={3} />
+        <StepBar stateNum={4} />
+
+      </div>
     </div>
   );
 }
