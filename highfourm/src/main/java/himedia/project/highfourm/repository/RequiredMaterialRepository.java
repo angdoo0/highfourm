8a7158d3e36f8f0bfd25583e4b8b984a7719df22
Wooyanggang <didrod6589@naver.com>
2024-02-20 16:27:14 +0900

    Revert "🚨작업 실적 등록 오류 핫픽스"
    This reverts commit f089f46a9f8c8bccc6d7176563279a7224dc3da5.

--- highfourm/src/main/java/himedia/project/highfourm/repository/RequiredMaterialRepository.java
+++ highfourm/src/main/java/himedia/project/highfourm/repository/RequiredMaterialRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import himedia.project.highfourm.entity.RequiredMaterial;
 import himedia.project.highfourm.entity.pk.RequiredMaterialPK;
@@ -12,6 +11,6 @@ import himedia.project.highfourm.entity.pk.RequiredMaterialPK;
 public interface RequiredMaterialRepository extends JpaRepository<RequiredMaterial, RequiredMaterialPK>{
 
 	@Query("SELECT rm FROM RequiredMaterial rm WHERE rm.requriedMaterialPK.product.productId = :productId")
-    List<RequiredMaterial> findAllByProductId(@Param("productId") String productId);
+    List<RequiredMaterial> findAllByProductId(String productId);
 
 }
